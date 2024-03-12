603046c2da363bf6d96a60efbbe89ec02d4d082d
Usuario <Usuario@CPD02-M15>
2024-03-12 11:46:00 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppAprovação/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAprovação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a nota do aluno?: ");
+            double n = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quanto o aluno tem de frequência?: ");
+            double f = double.Parse(Console.ReadLine());
+
+            if (n > 4 && f > 75) 
+            { 
+                Console.WriteLine("O aluno foi aprovado! "); 
+            }
+            else if (n > 4 && f < 75)
+             {
+                Console.WriteLine("O aluno está de recuperação! ");
+            }
+            else
+            { 
+                Console.WriteLine("O aluno está retido! ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
